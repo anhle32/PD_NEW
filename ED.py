77b6ba3416b14e3800068eada615f9f4b50122d7
anhle32 <90089985+anhle32@users.noreply.github.com>
2024-03-25 10:53:21 +0700

    Update ED.py

--- ED.py
+++ ED.py
@@ -57,6 +57,9 @@ if uploaded_file is not None:
 X = df.drop(columns=['default','LGD','EAD'])
 y = df['default']
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
 models = {
     'Logistic Regression': LogisticRegression(random_state=42),
     'Decision Tree': DecisionTreeClassifier(random_state=42),
